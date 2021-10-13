72e3716ad2eeae025a2c90e4541ab497e1a26ec1
Miguel Nequiz <nekixito@gmail.com>
2021-10-12 20:47:26 -0500

    CRUD completo de Cliente en service

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
@@ -7,4 +7,10 @@ import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 public interface IClienteService {
 	
 	public List<Cliente> findAll();
+	
+	public Cliente findById(Long id);
+	
+	public Cliente save(Cliente cliente);
+	
+	public void delete(Long id);
 }
